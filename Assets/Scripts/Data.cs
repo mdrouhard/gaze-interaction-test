925ab3ce96a1ae36f85d0000fe95e4b1b0cebec9
Meg Drouhard <drouhardmg@ornl.gov>
2015-07-09 15:52:13 -0400

    Updated Data script with random separate children objects

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -7,14 +7,17 @@ public class Data : MonoBehaviour {
 	public Mesh[] meshes;
 	public Material[] materials;
 
-	public int maxDepth;
-	private int depth;
-	
-	public float childScale;
-	public float spawnProbability;
-	public float maxRotationSpeed;
-	
-	private float rotationSpeed;
+	public int numNodes;
+	public float dataRange;
+
+//	public int maxDepth;
+//	private int depth;
+//	
+//	public float childScale;
+//	public float spawnProbability;
+//	public float maxRotationSpeed;
+//	
+//	private float rotationSpeed;
 	
 	private static Vector3[] childDirections = {
 		Vector3.up,
@@ -38,39 +41,49 @@ public class Data : MonoBehaviour {
 			meshes[Random.Range(0, meshes.Length)];
 		gameObject.AddComponent<MeshRenderer> ().material = 
 			materials [Random.Range(0, materials.Length)];
-		if (depth < maxDepth) {
-			CreateChildren();
+
+		for (int i = 0; i < (numNodes - 1); i++) {
+			CreateChild();
 		}
-		
-		rotationSpeed = Random.Range (-maxRotationSpeed, maxRotationSpeed);
+
+//		if (depth < maxDepth) {
+//			CreateChildren();
+//		}
+//		
+//		rotationSpeed = Random.Range (-maxRotationSpeed, maxRotationSpeed);
 	}
-	
-	private void Initialize(Data parent, int childIndex) {
+
+	private void CreateChild() {
+		new GameObject("Child").AddComponent<Data> ().
+			Initialize (this /*, i */);
+	}
+
+	private void Initialize(Data parent /*, int childIndex */) {
 		meshes = parent.meshes;
 		materials = parent.materials;
-		maxDepth = parent.maxDepth;
-		maxRotationSpeed = parent.maxRotationSpeed;
-		depth = parent.depth + 1;
-		spawnProbability = parent.spawnProbability;
-		childScale = parent.childScale;
+//		maxDepth = parent.maxDepth;
+//		maxRotationSpeed = parent.maxRotationSpeed;
+//		depth = parent.depth + 1;
+//		spawnProbability = parent.spawnProbability;
+//		childScale = parent.childScale;
 		transform.parent = parent.transform;
-		transform.localScale = Vector3.one * childScale;
-		transform.localPosition = childDirections[childIndex] * 
-			(Random.Range (1f, 5f) + 0.5f * childScale);
-		transform.localRotation = childOrientations[childIndex];
+//		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = childDirections[Random.Range (0,childDirections.Length)] * 
+			(Random.Range (1f, dataRange) + 0.5f /* * childScale */);
+//		transform.localRotation = childOrientations[childIndex];
 	}
 	
-	private void CreateChildren() {
-		for (int i = 0; i < childDirections.Length; i++) {
-			if(Random.value < spawnProbability) {
-				new GameObject ("Child").AddComponent<Data> ().
-					Initialize (this, i);
-			}
-		}
-	}
+//	private void CreateChildren() {
+//		for (int i = 0; i < childDirections.Length; i++) {
+//			if(Random.value < spawnProbability) {
+//				new GameObject ("Child").AddComponent<Data> ().
+//					Initialize (this, i);
+//			}
+//		}
+//	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (0f, rotationSpeed * Time.deltaTime, 0f);
+//		transform.Rotate (0f, rotationSpeed * Time.deltaTime, 0f);
 	}
 }
